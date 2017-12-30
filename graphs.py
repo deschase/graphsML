fe627056b93d674f0972249e59e60f51591f17b4
JAMJU <juliette.milletviolette@wanadoo.fr>
2017-12-30 19:35:29 +0100

    add community algo already coded + plot

--- graphs.py
+++ graphs.py
@@ -66,77 +66,103 @@ class Graph_community(object):
         # show graph
         plt.show()
 
-    # A debugger
+    def draw_D(self):
+        """ This function is going to draw D"""
+        graph_pos = nx.spring_layout(self.D)
+
+        # draw nodes, edges and labels
+        nx.draw_networkx_nodes(self.D, graph_pos, node_size=100, node_color='orange', alpha=0.3)
+        # we can now added edge thickness and edge color
+        nx.draw_networkx_edges(self.D, graph_pos, width=1, alpha=0.3, edge_color='b')
+        nx.draw_networkx_labels(self.D, graph_pos, font_size=6, font_family='sans-serif')
+
+        # show graph
+        plt.show()
+
+    def draw_E(self):
+        """ This function is going to draw E with arrows indicating their orientations"""
+        graph_pos = nx.spring_layout(self.E)
+
+        # draw nodes, edges and labels
+        nx.draw_networkx_nodes(self.E, graph_pos, node_size=100, node_color='orange', alpha=0.3)
+        # we can now added edge thickness and edge color
+        nx.draw_networkx_edges(self.E, graph_pos, width=1, alpha=0.3, edge_color='b', arrows = True)
+        nx.draw_networkx_labels(self.E, graph_pos, font_size=6, font_family='sans-serif')
+
+        # show graph
+        plt.show()
+
     def compute_E(self):
-        "To complete : add direction"
+        """Compute the exact directed graph described in the article"""
         list_edges = list(self.graph.edges)
         for link in list_edges:
             if link[0] != link[1]: # check not self loop
                 self.E.add_node(link)
         lg_node = list(self.E.nodes)
 
-        g2.node[2]['weight']
-
         # Calculation of the strength of each node
-        s = dict{}
+        s = dict()
         for pre_node in list(self.graph.nodes):
             s[pre_node] = 0
-            for ngh in list(self.graph.neighbors(extr1))
-                s[pre_node] += self.graph[pre_node][ngh]['weight']
+            for ngh in list(self.graph.neighbors(pre_node)):
+                if ngh != pre_node:
+                    s[pre_node] += self.graph[pre_node][ngh]['weight']
 
+        # Creation of the edges
         for nd in lg_node:
             extr1 = nd[0]
             extr2 = nd[1]
-            k1 = len(list(self.graph.neighbors(extr1)))
-            k2 = len(list(self.graph.neighbors(extr2)))
             wgt_alpha = self.graph[extr1][extr2]['weight']
-
             for ngbr1 in self.graph.neighbors(extr1):
-                if (extr1, ngbr1) in lg_node:
-                    wgt_beta = self.graph[extr1][ngbr1]['weight']
-                    self.E.add_edge(nd, (extr1, ngbr1),
-                                    weight = wgt_alpha/(s[extr1] - wgt_beta))
-                elif (ngbr1, extr1) in lg_node:
-                    wgt_beta = self.graph[ngbr1][extr1]['weight']
-                    self.E.add_edge(nd, (ngbr1, extr1),
-                                    weight = wgt_alpha/(s[extr1] - wgt_beta))
+                if (extr1, ngbr1) != (extr1, extr2) and (ngbr1, extr1) != (extr2, extr1):  # to avoid self loop
+                    if (extr1, ngbr1) in lg_node:
+                        wgt_beta = self.graph[extr1][ngbr1]['weight']
+                        self.E.add_edge(nd, (extr1, ngbr1),
+                                        weight = float(wgt_alpha)/float(s[extr1] - wgt_beta))
+                    elif (ngbr1, extr1) in lg_node:
+                        wgt_beta = self.graph[ngbr1][extr1]['weight']
+                        self.E.add_edge(nd, (ngbr1, extr1),
+                                        weight = float(wgt_alpha)/float(s[extr1] - wgt_beta))
             for ngbr2 in self.graph.neighbors(extr2):
-                if (extr2, ngbr2) in lg_node:
-                    wgt_beta = self.graph[extr2][ngbr2]['weight']
-                    self.E.add_edge(nd, (extr2, ngbr2),
-                                    weight = wgt_alpha/(s[extr2] - wgt_beta))
-                elif (ngbr2, extr2) in lg_node:
-                    wgt_beta = self.graph[ngbr2][extr2]['weight']
-                    self.E.add_edge(nd, (ngbr2, extr2),
-                                    weight = wgt_alpha/(s[extr2] - wgt_beta))
+                if (ngbr2, extr2) != (extr1, extr2) and (extr2, ngbr2) != (extr2, extr1):  # to avoid self loop
+                    if (extr2, ngbr2) in lg_node:
+                        wgt_beta = self.graph[extr2][ngbr2]['weight']
+                        self.E.add_edge(nd, (extr2, ngbr2),
+                                        weight = float(wgt_alpha)/float(s[extr2] - wgt_beta))
+                    elif (ngbr2, extr2) in lg_node:
+                        wgt_beta = self.graph[ngbr2][extr2]['weight']
+                        self.E.add_edge(nd, (ngbr2, extr2),
+                                        weight = float(wgt_alpha)/float(s[extr2] - wgt_beta))
 
     def compute_D(self):
-        list_edges = list(self.graph.edges)
+        list_edges = list(self.graph.edges) #get the list of edges of the original graph
 
-        for link in list_edges:
+        for link in list_edges: # create the list of nodes of D (the edges of the original graph)
             if link[0] != link[1]: # check not self loop
                 self.D.add_node(link)
                 self.Dlist.append(link)
 
-        lg_node = list(self.D.nodes)
+        lg_node = list(self.D.nodes) # get the list of nodes of the graph D (line graph)
 
-        for nd in lg_node:
-            extr1 = nd[0]
+        for nd in lg_node: # for each node of D
+            extr1 = nd[0] # get the 2 vertex of the original edge
             extr2 = nd[1]
-            k1 = len(list(self.graph.neighbors(extr1)))
+            k1 = len(list(self.graph.neighbors(extr1))) # get the degree of each original node
             k2 = len(list(self.graph.neighbors(extr2)))
             if k1 != 1:
-                for ngbr1 in self.graph.neighbors(extr1):
-                    if (extr1, ngbr1) in lg_node:
-                        self.D.add_edge(nd, (extr1, ngbr1), weight = 1./(k1 -1) )
-                    elif (ngbr1, extr1) in lg_node:
-                        self.D.add_edge(nd, (ngbr1, extr1), weight = 1./(k1 -1))
+                for ngbr1 in self.graph.neighbors(extr1): # get the neighbours of one of the original vertex
+                    if (extr1, ngbr1) != (extr1, extr2) and (ngbr1, extr1) != (extr2, extr1): # to avoid self loop
+                        if (extr1, ngbr1) in lg_node:
+                            self.D.add_edge(nd, (extr1, ngbr1), weight = 1./(k1 -1) )
+                        elif (ngbr1, extr1) in lg_node:
+                            self.D.add_edge(nd, (ngbr1, extr1), weight = 1./(k1 -1))
             if k2 != 1:
                 for ngbr2 in self.graph.neighbors(extr2):
-                    if (extr2, ngbr2) in lg_node:
-                        self.D.add_edge(nd, (extr2, ngbr2), weight = 1./(k2 -1))
-                    elif (ngbr2, extr2) in lg_node:
-                        self.D.add_edge(nd, (ngbr2, extr2), weight = 1./(k2 -1))
+                    if (ngbr2, extr2) != (extr1, extr2) and (extr2, ngbr2) != (extr2, extr1):  # to avoid self loop
+                        if (extr2, ngbr2) in lg_node:
+                            self.D.add_edge(nd, (extr2, ngbr2), weight = 1./(k2 -1))
+                        elif (ngbr2, extr2) in lg_node:
+                            self.D.add_edge(nd, (ngbr2, extr2), weight = 1./(k2 -1))
 
     def return_total_weight(self, D, E):
         weight = 0
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import sys
 
 import matplotlib.pyplot as plt
 import networkx as nx
-
 import graphs as gr
+import community
 import community_detection as com
 
-
+colors = ['blue', 'orange', 'purple', 'green', 'black', 'pink', 'brown', 'magenta', 'grey', 'cyan']
 def create_line_graph(graph):
     line_graph = nx.Graph()
     list_edges = list(graph.edges)
@@ -32,9 +32,32 @@ def create_line_graph(graph):
 # gr2 = create_line_graph(gr1)
 # draw_graph(gr2)
 
-original= gr.Graph_community([1,2,3,4,5,6,7,8])
-print(incidence_matrix(original))
+original= gr.Graph_community([1])
 original.draw_graph()
 original.compute_D()
-label = com.spectral_clustering(original.D, 'rw')
-original.draw_graph_com( label, original.Dlist)
+original.compute_E()
+"""weight = list(original.E.edges(data=True))
+for w in weight:
+    print w
+original.draw_E()"""
+
+#first compute the best partition
+partition = community.best_partition(original.E.to_undirected()) # algo de louvain with weighted original graph
+#partition = community.best_partition(original.D) # algo de louvain with unweighted original graph
+
+#drawing : we draw each community one by one
+"""size = float(len(set(partition.values())))
+pos = nx.circular_layout(original.graph)
+count = 0
+for com in set(partition.values()) :
+    count = count + 1.
+    list_edges = [edge for edge in partition.keys()
+                                if partition[edge] == com]
+    nx.draw_networkx_nodes(original.graph, pos, node_size=150, node_color='orange', alpha=0.3)
+    nx.draw_networkx_labels(original.graph, pos, font_size=10, font_family='sans-serif')
+    nx.draw_networkx_edges(original.graph, pos, edgelist = list_edges, alpha = 1, edge_color=colors[int(count)])
+    plt.show()"""
+
+
+
+
