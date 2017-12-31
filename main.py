897d5fdb074bbbcdd29395036bf4c554cd55e54e
deschase <sebastien.deschamps@eleves.enpc.fr>
2017-12-31 13:46:32 +0100

    Data tome 10

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import community_detection as com
 # gr2 = create_line_graph(gr1)
 # draw_graph(gr2)
 
-original= gr.Graph_community([1, 2, 3, 4, 5, 6 , 7, 8,9])
+original= gr.Graph_community([1,2,3,4,5,6,7,8,9,10])
 original.draw_graph()
 original.compute_D()
 original.compute_E()
@@ -24,8 +24,3 @@ for w in weight:
     print w"""
 
 original.draw_graph_com(D = False, E = True)
-
-
-
-
-
